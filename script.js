b2981381ebf275d0b1de8c398a8f240686fbc893
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-06-06 19:40:10 +0100

    html

---
+++ script.js
@@ -0,0 +1,20 @@
+/* global variable 
+search history array
+link weather api
+weather api key
+*/
+
+/* dom elements
+grab search form
+grab search input
+grab container for todays weather
+grab 5 day forecast
+grab search history buttons
+*/
+
+/* function to grab search history
+for loop to create buttons for previously searched cities
+another function to update locas storage based on search history 
+function to get search history from local storage
+creating html elements and workin w api
+*/
