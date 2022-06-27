13ce06b7607385636e5f9594de676f624af5fcc0
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-06-27 21:24:00 +0100

    Making recent searches

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,73 @@
+//target parent
+    const recentSearchesContainer = $("#recent-searches-container");
+
+const readFromLocalStorage = (key, defaultValue) => {
+    // get from LS using key name
+    const dataFromLS = localStorage.getItem(key);
+  
+    // parse data from LS
+    const parsedData = JSON.parse(dataFromLS);
+  
+    if (parsedData) {
+      return parsedData;
+    } else {
+      return defaultValue;
+    }
+  };
+
+const renderRecentSearches = () => {
+
+    // get recent searches from LS
+    // const recentSearches = readFromLocalStorage("recentSearches",[]);
+    const recentSearches = ["London", "Leeds","BHam"]
+
+    //("foo","bar")
+    if(recentSearches.length) {
+
+        const createRecentCity = (city) => {
+            return `<li class="list-group-item" data-city="${city}"> ${city}</li>`
+        };
+
+        const recentCities = recentSearches.map(createRecentCity).join("");
+
+        console.log(recentCities)
+    //else render recent searches list
+    const ul = `<ul class="list-group">
+    ${recentCities}
+  </ul>`;
+
+  // append to parent
+  recentSearchesContainer.append(ul);
+
+    } else {
+        //if empty show alert
+    const alert = `<div class ="alert alert-warning" role="alert">
+            No searches
+        </div>`;
+
+        // append to parent
+        recentSearchesContainer.append(alert);
+    }
+};
+
+const handleRecentSearchClick =(event) => {
+    const target = $(event.target);
+
+    // restrict clicks only from li's
+    if (target.is("li")) {
+        // get data city attribute
+        const cityName = target.attr("data-city");
+        console.log(cityName);
+    }
+};
+
+const onReady = () => {
+    renderRecentSearches();
+}; 
+
+recentSearchesContainer.click(handleRecentSearchClick)
+$(document).ready(onReady)
+
 // global variable 
 var button = $('#find-city');
 // var cityInput = $('#city-input')
@@ -21,34 +91,33 @@ button.click((function(){
 }));
 
 
-//function to show saved city button after refresh
-function showSavedData() {
-    var cityArr = JSON.parse(localStorage.getItem('citylist'));
+// //function to show saved city button after refresh
+// function showSavedData() {
+//     var cityArr = JSON.parse(localStorage.getItem('citylist'));
 
 
-    for (var i = 0; i < cityArr.length; i++) {
-        console.log("cityArr", cityArr);
+//     for (var i = 0; i < cityArr.length; i++) {
+//         console.log("cityArr", cityArr);
 
-         // Then dynamicaly generating buttons for each city in the array
-         var a = $("<button>").attr({ "class": "list-group-item list-group-item-action", "id": cityArr[i] });
+//          // Then dynamicaly generating buttons for each city in the array
+//          var a = $("<button>").attr({ "class": "list-group-item list-group-item-action", "id": cityArr[i] });
 
-         // Providing the initial button text
-         a.text(cityArr[i]);
-         // Adding the button to the buttons-view div
-         $("#buttons-view").append(a);
+//          // Providing the initial button text
+//          a.text(cityArr[i]);
+//          // Adding the button to the buttons-view div
+//          $("#buttons-view").append(a);
  
-         $("#" + cityArr[i]).on("click", function (event) {
-             event.preventDefault();
+//          $("#" + cityArr[i]).on("click", function (event) {
+//              event.preventDefault();
  
-             var cityName = this.id;
+//              var cityName = this.id;
  
-             getWeatherToday(cityName, "existing");
-             getWeatherForecast(cityName, APIKey);
+//              getWeatherToday(cityName, "existing");
+//              getWeatherForecast(cityName, APIKey);
  
  
-         });
-    }
-}
+//          });
+//     }
 // function to generate cards
 function makeCards (data){
     console.log(data.weather)
